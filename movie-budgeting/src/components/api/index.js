efc85c457a8fa867649acb632b0a766ec9e70b74
seaybx <sandeep.paul@gmail.com>
2020-05-26 22:36:57 +0530

    Added Passport login, authentication and projects module

--- movie-budgeting/src/components/api/index.js
+++ movie-budgeting/src/components/api/index.js
@@ -1,20 +1,60 @@
-import axios from 'axios';
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const api = axios.create({
 	baseURL:'http://localhost:5000/api'
 })
 
-export const createScene = payload => api.post('/create',payload);
-export const getAllScenes = () => api.get('/scenes');
-export const getSceneById = id => api.get(`/scene/${id}`);
-export const deleteScene = id => api.delete(`/deletescene/${id}`);
-export const updateScene = (id, payload) => api.put(`/scene/${id}`, payload);
 
-export const listUsers = () => api.get('/users');
-export const addUser = payload => api.post('/adduser',payload);
-export const getUserInfo = id => api.get(`user/${id}`);
-export const deleteUser = id => api.delete(`/deleteuser/${id}`);
+// Add 403 response interceptor
+api.interceptors.response.use(undefined, err => {
+    const error = err.response;
+    console.log("Interceptor Error log", error)
+    if (error.status===403) {
+    	Swal.fire({
+            title: "Session Expired",
+            text: "Your session has expired. You will be redirected to the login page",
+            type: "warning"
+        })
+        localStorage.removeItem('token')
+        localStorage.removeItem('userid')
+        localStorage.removeItem('user')
+    	window.location.href="/login/#/login"
+     }
+     return Promise.reject(err);
+  });
+
+  export const sessionValid = () => {
+    let token = localStorage.getItem('token')
+    if(token !== null){
+         api.defaults.headers.common['Authorization'] = `Basic ${token}`;
+      return true
+    } else {
+      window.location.href="/login/#/login";
+      return false
+    }
+  }
+    
+
+export const createScene = payload => api.post('/create',payload)
+export const getAllScenes = () => api.get('/scenes')
+export const getSceneById = id => api.get(`/scene/${id}`)
+export const deleteScene = id => api.delete(`/deletescene/${id}`)
+export const updateScene = (id, payload) => api.put(`/scene/${id}`, payload)
+
+export const listUsers = () => api.get('/users')
+export const addUser = payload => api.post('/adduser',payload)
+export const getUserInfo = id => api.get(`user/${id}`)
+export const deleteUser = id => api.delete(`/deleteuser/${id}`)
 export const updateUser = (id, payload) => api.put(`user/${id}`, payload)
+
+export const login = payload => api.post('/login', payload)
+
+export const addProject = payload => api.post('/createproject', payload)
+export const deleteProject = id => api.delete(`/deleteproject/${id}`)
+export const listProjects = () => api.get('/projects')
+export const getProjectInfo = id => api.get(`project/${id}`)
+export const updateProject = (id, payload) => api.put(`project/${id}`, payload)
 
 const apis ={
 	createScene,
@@ -26,7 +66,14 @@ const apis ={
 	addUser,
 	getUserInfo,
 	updateUser,
-	deleteUser
+	deleteUser,
+	login,
+	sessionValid,
+	addProject,
+	listProjects,
+	deleteProject,
+	getProjectInfo,
+	updateProject
 }
 
 export default apis;
--- movie-budgeting/src/components/app/App.js
+++ movie-budgeting/src/components/app/App.js
@@ -1,44 +1,109 @@
 import React from 'react';
 import './App.css';
-import {HashRouter, Route} from 'react-router-dom';
+import {HashRouter, Route, Redirect} from 'react-router-dom';
 import Sidebar from '../sidebar/Sidebar';
 import CreateScene from '../new-scene/new-scene.js';
 import SceneDetails from '../scene-details/scene-details.js';
-import CreateProject from '../create-project/create-project.js';
+import CreateProject from '../project/create-project.js';
 import UpdateScene from '../scene-details/update-scene.js';
 import ListUser from '../users/list-users.js';
 import AddUser from '../users/add-user.js'
 import UpdateUser from '../users/update-user.js'
+import UserPanel from '../users/user-panel.js'
 import Login from '../login/login.js'
+import Dashboard from '../dashboard/dashboard.js'
+import ListProjects from '../project/list-projects.js'
+import UpdateProject from '../project/update-project.js'
+import api from '../api';
 
-function App() {
 
-  let userLoggedIn = false;
 
-  if(!userLoggedIn) {
-    window.location.href= '#/login'
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+
+    return (
+
+        // Show the component only when the user is logged in
+        // Otherwise, redirect the user to /signin page
+          
+        <Route {...rest} render={props => (
+            api.sessionValid() ?
+                <Component {...props} />
+            : <Redirect to="/login" />
+        )} />
+    );
+};
+
+const PublicRoute = ({component: Component, restricted, ...rest}) => {
+    return (
+        // restricted = false meaning public route
+        // restricted = true meaning restricted route
+        <Route {...rest} render={props => (
+            api.sessionValid() && restricted ?
+                <Redirect to="dashboard/#/dashboard" />
+            : <Component {...props} />
+        )} />
+    );
+};
+
+
+export default class App extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        isLoggedIn : false,
+        user: {}
+      }
+    }
+
+  
+    render() {
+
+      let isLoggedIn = this.state.isLoggedIn;
+
+      if(api.sessionValid()) {
+         isLoggedIn = true
+      }
+
+      return (
+
+      <div className="App">
+        <header className="App-header">
+          <h1>Budgeting- Scene by Scene </h1>
+        </header>
+        <HashRouter>
+      {isLoggedIn 
+        ?
+          <React.Fragment>
+            <div id="main-content"> 
+                <PrivateRoute  path="/project/create-project" component={CreateProject} />
+                <PrivateRoute  path="/new-scene" exact component={CreateScene} />
+                <PrivateRoute  path="/scene/:id" exact component={SceneDetails} />
+                <PrivateRoute  path="/updatescene/:id" exact component={UpdateScene} />
+                <PrivateRoute  path="/users/" exact component={ListUser} />
+                <PrivateRoute  path="/adduser/" exact component={AddUser} />
+                <PrivateRoute  path="/updateuser/:id" exact component={UpdateUser} />
+                <PrivateRoute  path="/dashboard" exact component={Dashboard} />
+                <PrivateRoute  path="/projects" exact component={ListProjects} />
+                <PrivateRoute  path="/updateproject/:id" exact component={UpdateProject} />
+
+                <UserPanel />
+
+              </div>
+            <div id="sidebar"> <Sidebar /> </div>
+          </React.Fragment> 
+        :
+          <div id="public-content">
+            <PublicRoute restricted={true} exact path="/login" component={Login} />
+            <UserPanel />
+          </div>
+         }
+        </HashRouter>     
+      </div>
+      )
+    }
   }
 
-  return (
-    <div className="App">
-		<HashRouter>
-			<header className="App-header">
-				<h1> Budgeting- Scene by Scene </h1>
-    		</header>
-     		<div id="main-content"> 
-          <Route path="/project/create-project" component={CreateProject} />
-     			<Route path="/new-scene" exact component={CreateScene} />
-          <Route path="/scene/:id" exact component={SceneDetails} />
-          <Route path="/updatescene/:id" exact component={UpdateScene} />
-          <Route path="/users/" exact component={ListUser} />
-          <Route path="/adduser/" exact component={AddUser} />
-          <Route path="/updateuser/:id" exact component={UpdateUser} />
-          <Route path="/login" exact component={Login} />
-     		</div>
-      	</HashRouter>
-          <div id="sidebar"> <Sidebar /> </div>       
-    </div>
-  );
-}
+ 
 
-export default App;
+
